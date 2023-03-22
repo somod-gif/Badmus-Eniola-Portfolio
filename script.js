4708239a894f6e69246cd368e44b199901e68d25
Badmus Eniola <115708246+somod-gif@users.noreply.github.com>
2023-03-22 14:40:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function dm() {
+    if (document.body.style.backgroundColor == "black") {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    }
+    // else {
+    //   document.body.style.backgroundColor = "white";
+    //   document.body.style.color = "black";
+    // }
+}
